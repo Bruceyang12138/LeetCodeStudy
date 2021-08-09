984e0416f904619c1a5d53d64f477a5996c33399
Bruceyang12138 <1925622127@qq.com>
2021-08-09 11:17:40 +0800

    好多次了，好麻烦

--- src/移除元素_27.java
+++ src/移除元素_27.java
@@ -2,37 +2,32 @@ public class 移除元素_27 {
 
 //可以先使用交换排序，让数组变为有序表
 public static void main(String[] args) {
-    int[] nums={6,6};
-    System.out.println("最终长度为="+removeElement(nums,6));
+    int[] nums={3,2,2,3};
+    System.out.println("最终长度为="+removeElement(nums,3));
 }
 
     public  static int removeElement(int[] nums, int val){
-        int temp;
         int len=nums.length;//最终长度
-        for(int i=0 ;i+1<len;i++){
-            //有序表内两个相邻元素相等，删去后面那个
-
+        for(int i=0 ;i<len;i++){
             if(nums[i]==val ){
-                for(int j=i;j+1<len;j++){
+                len=len-1;
+                System.out.println("数组长度已减一,现在为"+len);
+                //为了保证全部数组元素都向前移动了，
+                for(int j=i;j+1<nums.length;j++){
                     nums[j]=nums[j+1];
                 }
-
-                len=len-1;
                 i--;
-                System.out.println("数组长度已减一,现在为"+len);
+
                 //出现相同时.i当然要回退一位，方便去判断下一个是否相同
-            }else{
-                return 0;
             }
         }
         System.out.println("现在的len为 "+len);
         if((nums[0] ==val && len==1) || len==0){
             return 0;
         }
-        for(int i =0;i<len-1;i++){
+        for(int i =0;i<nums.length;i++){
             System.out.println(nums[i]);
         }
-
         return  len;
     }
 }
