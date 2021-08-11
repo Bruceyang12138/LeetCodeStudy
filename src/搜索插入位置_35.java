e4f8eae807f884e221b70cf0a8feec037b1f4eb6
Bruceyang12138 <1925622127@qq.com>
2021-08-11 09:33:12 +0800

    第二次

--- src/搜索插入位置_35.java
+++ src/搜索插入位置_35.java
@@ -1,7 +1,7 @@
 public class 搜索插入位置_35 {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4,5, 6, 7, 8, 9};
-        System.out.println(searchInsert(nums,9));
+        int[] nums = {1,3};
+        System.out.println(searchInsert(nums,0));
     }
 
     //有序表，要求时间复杂的为O（log n），想到用栈递归二分查找
@@ -19,14 +19,15 @@ public class 搜索插入位置_35 {
             return mid;
         }
         if (low >= high) {
-            if (target > nums[high]) {
-                return high+1;
-            }else if(target<nums[low]){
-                return low;
-            }else{
+            if(high<0){high=0;}
+            if(low>nums.length-1){low=nums.length;};
+            if (target > nums[low]) {
+                return low+1;
+            }else if(target<nums[high]){
                 return high;
+            }else{
+                return low;
             }
-
 
         } else if (target > nums[mid]) {
             return binarySearch(nums, mid + 1, high, target);
